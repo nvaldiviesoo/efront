80720e4277703ee4664d2ec4d34323d9a7971e84
Rlopeze <mumolopeze@uc.cl>
2024-05-20 23:29:00 -0400

    added condtinons to carouselLoader

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,15 @@ import { useGetProductsQuery } from './api/productsApi';
 
 export default function Home() {
 
-  const { data, isLoading } = useGetProductsQuery('');
+  const { data, isLoading, isError } = useGetProductsQuery('');
 
   return (
     <Layout>
       <div className='mx-10'>
         <h1 className='text-2xl font-bold'>20% DESCUENTO EN LOS ESENCIALES</h1>
-          {isLoading ? (
-            <CarouselLoader />
-          ) : (
-            <CarouselPlugin products={data.data} />
-          )}
+          { isLoading &&  <CarouselLoader /> }
+          { isError && <h1>Ha ocurrido un error 😅</h1>}
+          {data && !isLoading && <CarouselPlugin products={data.data} />}
       </div>
     </Layout>
   );
