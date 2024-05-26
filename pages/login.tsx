b07175d6ffe0ccbf5f340e6c935541e4ac73e34a
Rlopeze <69649817+Rlopeze@users.noreply.github.com>
2024-05-26 12:01:44 -0400

    feat(login): added login and registration (#5)

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { Separator } from '@radix-ui/react-separator';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { useDispatch } from 'react-redux';
+
+import { useState } from 'react';
+import { Separator } from '@radix-ui/react-separator';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 
-import { Tabs, TabsList, TabsTrigger } from '../components/ui/tabs';
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from '../components/ui/tabs';
 import { Card, CardContent, CardHeader } from '../components/ui/card';
 import {
   Form,
@@ -17,22 +26,73 @@ import {
 } from '../components/ui/form';
 import { Input } from '../components/ui/input';
 import { Button } from '../components/ui/button';
+import { useLoginMutation, useRegisterMutation } from './api/authApi';
+import { setIsAuthenticated, setUser } from '../redux/features/userSlice';
 
-const formSchema = z.object({
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+const signupSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  lastName: z.string().min(1, 'Name is required'),
   email: z.string().email(),
   password: z.string().min(8),
 });
 
 const Login = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [activeTab, setActiveTab] = useState('login');
+
+  const loginForm = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: '',
       password: '',
     },
   });
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+
+  const signupForm = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      name: '',
+      lastName: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const [login, { isLoading: isLoginLoading, data }] = useLoginMutation();
+  const [signup, { isLoading: isSignupLoading, data: signupData }] =
+    useRegisterMutation();
+
+  function onLoginSubmit(values: z.infer<typeof loginSchema>) {
+    const loginData = {
+      email: values.email,
+      password: values.password,
+    };
+    login(loginData);
+  }
+  if (data) {
+    dispatch(setUser(data.data.user));
+    dispatch(setIsAuthenticated(true));
+    router.push('/');
+  }
+
+  function onSignupSubmit(values: z.infer<typeof signupSchema>) {
+    const registerData = {
+      email: values.email,
+      name: values.name + values.lastName,
+      password: values.password,
+    };
+    signup(registerData);
+  }
+  if (signupData) {
+    dispatch(setUser(signupData.data.user));
+    dispatch(setIsAuthenticated(true));
+    router.push('/');
   }
 
   return (
@@ -46,7 +106,11 @@ const Login = () => {
           <Image src='/Logo.jpeg' alt='Logo' width={200} height={200} />
         </CardHeader>
         <CardContent>
-          <Tabs defaultValue='login' className='w-[250px]'>
+          <Tabs
+            value={activeTab}
+            onValueChange={setActiveTab}
+            className='w-[250px]'
+          >
             <TabsList className='grid grid-cols-2 rounded-3xl bg-neutral-200'>
               <TabsTrigger className='rounded-3xl font-semibold' value='login'>
                 LOGIN
@@ -55,47 +119,126 @@ const Login = () => {
                 SIGN UP
               </TabsTrigger>
             </TabsList>
+            <TabsContent value='login'>
+              <Form {...loginForm}>
+                <form
+                  onSubmit={loginForm.handleSubmit(onLoginSubmit)}
+                  className='space-y-6'
+                >
+                  <FormField
+                    control={loginForm.control}
+                    name='email'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email Address</FormLabel>
+                        <FormControl>
+                          <Input placeholder='Enter Email Address' {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={loginForm.control}
+                    name='password'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder='Enter a Password'
+                            type='password'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <h2 className='pl-[30%] underline '>Forgot password?</h2>
+                  <Button
+                    className='w-full rounded-3xl bg-black'
+                    type='submit'
+                    disabled={isLoginLoading}
+                  >
+                    LOG IN
+                  </Button>
+                </form>
+              </Form>
+            </TabsContent>
+            <TabsContent value='signup'>
+              <Form {...signupForm}>
+                <form
+                  onSubmit={signupForm.handleSubmit(onSignupSubmit)}
+                  className='space-y-6'
+                >
+                  <FormField
+                    control={signupForm.control}
+                    name='name'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Nombre</FormLabel>
+                        <FormControl>
+                          <Input placeholder='Ingresa tu nombre' {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={signupForm.control}
+                    name='lastName'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Apellido</FormLabel>
+                        <FormControl>
+                          <Input placeholder='Ingresa tu apellido' {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={signupForm.control}
+                    name='email'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email Address</FormLabel>
+                        <FormControl>
+                          <Input placeholder='Enter Email Address' {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={signupForm.control}
+                    name='password'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder='Enter a Password'
+                            type='password'
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button
+                    className='w-full rounded-3xl bg-black'
+                    type='submit'
+                    disabled={isSignupLoading}
+                  >
+                    SIGN UP
+                  </Button>
+                </form>
+              </Form>
+            </TabsContent>
           </Tabs>
-        </CardContent>
-        <CardContent className='w-7/12'>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
-              <FormField
-                control={form.control}
-                name='email'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email Address</FormLabel>
-                    <FormControl>
-                      <Input placeholder='Enter Email Address' {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name='password'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder='Enter a Password'
-                        type='password'
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <h2 className='pl-[30%] underline '>Forgot password?</h2>
-              <Button className='w-full rounded-3xl bg-black' type='submit'>
-                LOG IN
-              </Button>
-            </form>
-          </Form>
         </CardContent>
       </Card>
     </div>
--- redux/store/store.ts
+++ redux/store/store.ts
@@ -1,14 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import shopCartReducer from '../features/shopCartSlice';
+import userReducer from '../features/userSlice';
 
 import { productApi } from '../../pages/api/productsApi';
+import { authApi } from '../../pages/api/authApi';
 
 export const store = configureStore({
   reducer: {
+    auth: userReducer,
     shopCart: shopCartReducer,
     [productApi.reducerPath]: productApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([productApi.middleware]),
+    getDefaultMiddleware().concat([productApi.middleware, authApi.middleware]),
 });
