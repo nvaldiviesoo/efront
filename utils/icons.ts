fb41d0a5a10dd06bc9527d7f86982245e2df39ab
Nicolaus Valdivieso <nvaldivieso@uc.cl>
2024-06-17 10:45:53 -0400

    Feat(admin): Most important things at frontend level ready

--- utils/icons.ts
+++ utils/icons.ts
@@ -1,5 +1,15 @@
-import { IoPersonOutline, IoSearchOutline } from 'react-icons/io5';
+import {
+  IoPersonOutline,
+  IoSearchOutline,
+  IoBarChartOutline,
+} from 'react-icons/io5';
 import { BsBag } from 'react-icons/bs';
 import { AiOutlineHeart } from 'react-icons/ai';
 
-export { IoPersonOutline, BsBag, AiOutlineHeart, IoSearchOutline };
+export {
+  IoPersonOutline,
+  BsBag,
+  AiOutlineHeart,
+  IoSearchOutline,
+  IoBarChartOutline,
+};
