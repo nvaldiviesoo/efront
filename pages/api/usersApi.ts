0bffb3a33b2d410b0a1b342dc6379d6c3093daa5
Nicolaus Valdivieso <nvaldivieso@uc.cl>
2024-06-18 22:04:05 -0400

    feat(users): Admin can set users to staff/not staff

--- pages/api/usersApi.ts
+++ pages/api/usersApi.ts
@@ -13,7 +13,7 @@ export const userApi = createApi({
         headers: {
           'Content-Type': 'application/json',
           Authorization:
-            // TODO: Change this to a real token
+            //  TODO: Change this to a real token
             'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzIwMDU3MzQ1LCJqdGkiOiI0NzQyOGI5NTNhMTU0MzRiODU4YjE3YjcxNDYwMWUyZiIsInVzZXJfaWQiOiI2MDRkMDNkNy0xYmVjLTQ5NjUtODA2OS03ZjRkZTMxYjM1NjIifQ.qVn0_Xp_rzXh5MusCWOgDkx_Rfx0sn3gg-chVeemEMo',
         },
       }),
