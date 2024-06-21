c92cac19da275fb8575eb8f5bf5f410444f0184e
Nicolaus Valdivieso <nvaldivieso@uc.cl>
2024-06-20 22:49:24 -0400

    Added products by category filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const { data, isLoading, isError } = useGetProductsQuery('');
   return (
     <Layout>
-      <div className='mx-10'>
+      <div className='mx-10 p-10'>
         <h1 className='text-2xl font-bold'>20% DESCUENTO EN LOS ESENCIALES</h1>
         {isLoading && <CarouselLoader />}
         {isError && <h1>Ha ocurrido un error 😅</h1>}
