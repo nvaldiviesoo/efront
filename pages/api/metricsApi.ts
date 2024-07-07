3818f3c4f8960a5e8ec58fed8917cccc18dc41c9
Nicolaus Valdivieso <nvaldivieso@uc.cl>
2024-07-07 17:36:33 -0400

    Fix headers

--- pages/api/metricsApi.ts
+++ pages/api/metricsApi.ts
@@ -1,14 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { useUserToken } from './tokenHooks';
 
 export const metricApi = createApi({
   reducerPath: 'metricApi',
   baseQuery: fetchBaseQuery({
     baseUrl: `${process.env.NEXT_PUBLIC_API_URL}/metrics`,
-    prepareHeaders: (headers) => {
+    prepareHeaders: (headers, { getState = useUserToken }) => {
       // TODO get token from local storage
-      const token = process.env.NEXT_PUBLIC_TOKEN;
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
+      // const token = process.env.NEXT_PUBLIC_TOKEN;
+      const user = getState();
+      if (user) {
+        headers.set('Authorization', `Bearer ${user.auth?.user.access}`);
       }
       return headers;
     },
